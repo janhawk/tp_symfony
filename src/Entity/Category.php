540ad9e4e5e8ce484bfcf500d38455952b165f49
janhawk <janlawrenceabalos@gmail.com>
2022-08-12 12:15:54 +0200

    creation of admin/categorys and admin/category/create

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,11 +2,12 @@
 
 namespace App\Entity;
 
-use App\Repository\CategoryRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
+use App\Entity\OrderDetail;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 class Category
@@ -16,24 +17,67 @@ class Category
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 45)]
+    #[ORM\Column(length: 100, unique: true)]
     private ?string $name = null;
 
-    #[ORM\Column(length: 45)]
+    #[ORM\Column(length: 100, unique: true)]
     private ?string $slug = null;
 
-    #[ORM\Column(length: 45, nullable: true)]
-    private ?string $img = null;
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $abstract = null;
 
-    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    #[ORM\Column(type: Types::TEXT)]
     private ?string $description = null;
 
-    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Product::class, orphanRemoval: true)]
-    private Collection $products;
+    #[ORM\Column]
+    private ?int $quantity = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column]
+    private ?int $min_players = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $max_players = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $minimum_age = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $duration = null;
+
+    #[ORM\Column(length: 45, nullable: true)]
+    private ?string $editor = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $theme = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $mecanism = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\ManyToOne(inversedBy: 'categorys')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $category = null;
+
+    #[ORM\OneToMany(mappedBy: 'category_id', targetEntity: OrderDetail::class)]
+    private Collection $orderDetails;
+
+    #[ORM\Column(length: 45)]
+    private ?string $img1 = null;
+
+    #[ORM\Column(length: 45, nullable: true)]
+    private ?string $img2 = null;
+
+    #[ORM\Column(length: 45, nullable: true)]
+    private ?string $img3 = null;
 
     public function __construct()
     {
-        $this->products = new ArrayCollection();
+        $this->orderDetails = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -65,14 +109,14 @@ class Category
         return $this;
     }
 
-    public function getImg(): ?string
+    public function getAbstract(): ?string
     {
-        return $this->img;
+        return $this->abstract;
     }
 
-    public function setImg(?string $img): self
+    public function setAbstract(string $abstract): self
     {
-        $this->img = $img;
+        $this->abstract = $abstract;
 
         return $this;
     }
@@ -82,39 +126,207 @@ class Category
         return $this->description;
     }
 
-    public function setDescription(?string $description): self
+    public function setDescription(string $description): self
     {
         $this->description = $description;
 
         return $this;
     }
 
-    /**
-     * @return Collection<int, Product>
-     */
-    public function getProducts(): Collection
+    public function getQuantity(): ?int
     {
-        return $this->products;
+        return $this->quantity;
     }
 
-    public function addProduct(Product $product): self
+    public function setQuantity(int $quantity): self
     {
-        if (!$this->products->contains($product)) {
-            $this->products->add($product);
-            $product->setCategory($this);
+        $this->quantity = $quantity;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getMinPlayers(): ?int
+    {
+        return $this->min_players;
+    }
+
+    public function setMinPlayers(int $min_players): self
+    {
+        $this->min_players = $min_players;
+
+        return $this;
+    }
+
+    public function getMaxPlayers(): ?int
+    {
+        return $this->max_players;
+    }
+
+    public function setMaxPlayers(?int $max_players): self
+    {
+        $this->max_players = $max_players;
+
+        return $this;
+    }
+
+    public function getMinimumAge(): ?int
+    {
+        return $this->minimum_age;
+    }
+
+    public function setMinimumAge(?int $minimum_age): self
+    {
+        $this->minimum_age = $minimum_age;
+
+        return $this;
+    }
+
+    public function getDuration(): ?\DateTimeInterface
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(?\DateTimeInterface $duration): self
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getEditor(): ?string
+    {
+        return $this->editor;
+    }
+
+    public function setEditor(?string $editor): self
+    {
+        $this->editor = $editor;
+
+        return $this;
+    }
+
+    public function getTheme(): ?string
+    {
+        return $this->theme;
+    }
+
+    public function setTheme(?string $theme): self
+    {
+        $this->theme = $theme;
+
+        return $this;
+    }
+
+    public function getMecanism(): ?string
+    {
+        return $this->mecanism;
+    }
+
+    public function setMecanism(?string $mecanism): self
+    {
+        $this->mecanism = $mecanism;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): self
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, OrderDetail>
+     */
+    public function getOrderDetails(): Collection
+    {
+        return $this->orderDetails;
+    }
+
+    public function addOrderDetail(OrderDetail $orderDetail): self
+    {
+        if (!$this->orderDetails->contains($orderDetail)) {
+            $this->orderDetails->add($orderDetail);
+            $orderDetail->setCategoryId($this);
         }
 
         return $this;
     }
 
-    public function removeProduct(Product $product): self
+    public function removeOrderDetail(OrderDetail $orderDetail): self
     {
-        if ($this->products->removeElement($product)) {
+        if ($this->orderDetails->removeElement($orderDetail)) {
             // set the owning side to null (unless already changed)
-            if ($product->getCategory() === $this) {
-                $product->setCategory(null);
+            if ($orderDetail->getCategoryId() === $this) {
+                $orderDetail->setCategoryd(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getImg1(): ?string
+    {
+        return $this->img1;
+    }
+
+    public function setImg1(string $img1): self
+    {
+        $this->img1 = $img1;
+
+        return $this;
+    }
+
+    public function getImg2(): ?string
+    {
+        return $this->img2;
+    }
+
+    public function setImg2(?string $img2): self
+    {
+        $this->img2 = $img2;
+
+        return $this;
+    }
+
+    public function getImg3(): ?string
+    {
+        return $this->img3;
+    }
+
+    public function setImg3(?string $img3): self
+    {
+        $this->img3 = $img3;
 
         return $this;
     }
