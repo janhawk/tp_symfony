d71de137a7124546e8fe027ddf6ce3325910aae8
janhawk <janlawrenceabalos@gmail.com>
2022-08-12 14:11:59 +0200

    fixe error categoryType

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,108 +2,17 @@
 
 namespace App\Controller;
 
-use DateTimeImmutable;
-use App\Entity\Category;
-use App\Form\CategoryType;
-use App\Repository\CategoryRepository;
-use Doctrine\Persistence\ManagerRegistry;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\AsciiSlugger;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CategoryController extends AbstractController
 {
-    #[Route('/category', name: 'categorys')]
-    public function index(CategoryRepository $categoryRepository): Response
+    #[Route('/category', name: 'app_category')]
+    public function index(): Response
     {
-        $categorys = $categoryRepository->findAll();
         return $this->render('category/index.html.twig', [
-            'categorys' => $categorys,
+            'controller_name' => 'CategoryController',
         ]);
     }
-
-    #[Route('/category/{slug}', name: 'category_show')]
-    public function show($slug, CategoryRepository $categoryRepository): Response
-    {
-        $category = $categoryRepository->findOneBy(['slug' => $slug]);
-        return $this->render('category/show.html.twig', [
-            'category' => $category
-        ]);
-    }
-
-    #[Route('/admin/categorys', name: 'admin_categorys')]
-    public function adminList(CategoryRepository $categoryRepository): Response
-    {
-        $categorys = $categoryRepository->findAll();
-        return $this->render('category/adminList.html.twig', [
-            'categorys' => $categorys
-        ]);
-    }
-
-    #[Route('/admin/category/create', name: 'category_create')]
-    public function create(Request $request, CategoryRepository $categoryRepository, ManagerRegistry $managerRegistry): Response
-    {
-        $category = new Category(); // création d'un nouveau produit
-        $form = $this->createForm(CategoryType::class, $category); // création d'un formulaire avec en paramètre le nouveau produit
-        $form->handleRequest($request); // gestionnaire de requêtes HTTP
-
-        if ($form->isSubmitted() && $form->isValid()) { // vérifie si le formulaire a été soumis et est valide
-
-            $categorys = $categoryRepository->findAll(); // récupère tous les produits en base de données
-            $categoryNames = []; // initialise un tableau pour les noms de produits
-            foreach ($categorys as $category) { // pour chaque produit récupéré
-                $categoryNames[] = $category->getName(); // stocke le nom du produit dans le tableau
-            }
-            if (in_array($form['name']->getData(), $categoryNames)) { // vérifie qsi le nom du produit à créé n'est pas déjà utilisé en base de données
-                $this->addFlash('danger', 'Le produit n\'a pas pu être créé : le nom de produit est déjà utilisé');
-                return $this->redirectToRoute('admin_categorys');
-            }
-
-            $infoImg1 = $form['img1']->getData(); // récupère les données du champ img1 du formulaire
-
-            if (empty($infoImg1)) { // vérifie la présence de l'image principale dans le formulaire
-                $this->addFlash('danger', 'Le produit n\'a pas pu être créé : l\'image principale est obligatoire mais n\'a pas été renseignée');
-                return $this->redirectToRoute('admin_categorys');
-            }
-
-            $extensionImg1 = $infoImg1->guessExtension(); // récupère l'extension de fichier de l'image 1
-            $nomImg1 = time() . '-1.' . $extensionImg1; // crée un nom de fichier unique pour l'image 1
-            $infoImg1->move($this->getParameter('category_image_dir'), $nomImg1); // télécharge le fichier dans le dossier adéquat
-            $category->setImg1($nomImg1); // définit le nom de l'image à mettre ne base de données
-
-            $infoImg2 = $form['img2']->getData();
-            if ($infoImg2 !== null) {
-                $extensionImg2 = $infoImg2->guessExtension();
-                $nomImg2 = time() . '-2.' . $extensionImg2;
-                $infoImg2->move($this->getParameter('category_image_dir'), $nomImg2);
-                $category->setImg2($nomImg2);
-            }
-
-            $infoImg3 = $form['img3']->getData();
-            if ($infoImg3 !== null) {
-                $extensionImg3 = $infoImg3->guessExtension();
-                $nomImg3 = time() . '-3.' . $extensionImg3;
-                $infoImg3->move($this->getParameter('category_image_dir'), $nomImg3);
-                $category->setImg3($nomImg3);
-            }
-
-            $slugger = new AsciiSlugger();
-            $category->setSlug(strtolower($slugger->slug($form['name']->getData()))); // génère un slug à partir du titre renseigné dans le formulaire
-            $category->setCreatedAt(new DateTimeImmutable());
-
-            $manager = $managerRegistry->getManager();
-            $manager->persist($category);
-            $manager->flush();
-
-            $this->addFlash('success', 'Le produit a bien été créé'); // message de succès
-            return $this->redirectToRoute('admin_categorys');
-        }
-
-        return $this->render('category/form.html.twig', [
-            'categoryForm' => $form->createView()
-        ]);
-    }
-   
 }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,12 +2,11 @@
 
 namespace App\Entity;
 
-use App\Entity\OrderDetail;
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-use App\Repository\CategoryRepository;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
 class Category
@@ -17,67 +16,24 @@ class Category
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 100, unique: true)]
+    #[ORM\Column(length: 45)]
     private ?string $name = null;
 
-    #[ORM\Column(length: 100, unique: true)]
+    #[ORM\Column(length: 45)]
     private ?string $slug = null;
 
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $abstract = null;
+    #[ORM\Column(length: 45, nullable: true)]
+    private ?string $img = null;
 
-    #[ORM\Column(type: Types::TEXT)]
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
     private ?string $description = null;
 
-    #[ORM\Column]
-    private ?int $quantity = null;
-
-    #[ORM\Column]
-    private ?float $price = null;
-
-    #[ORM\Column]
-    private ?int $min_players = null;
-
-    #[ORM\Column(nullable: true)]
-    private ?int $max_players = null;
-
-    #[ORM\Column(nullable: true)]
-    private ?int $minimum_age = null;
-
-    #[ORM\Column(type: Types::TIME_MUTABLE, nullable: true)]
-    private ?\DateTimeInterface $duration = null;
-
-    #[ORM\Column(length: 45, nullable: true)]
-    private ?string $editor = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $theme = null;
-
-    #[ORM\Column(length: 255, nullable: true)]
-    private ?string $mecanism = null;
-
-    #[ORM\Column]
-    private ?\DateTimeImmutable $created_at = null;
-
-    #[ORM\ManyToOne(inversedBy: 'categorys')]
-    #[ORM\JoinColumn(nullable: false)]
-    private ?Category $category = null;
-
-    #[ORM\OneToMany(mappedBy: 'category_id', targetEntity: OrderDetail::class)]
-    private Collection $orderDetails;
-
-    #[ORM\Column(length: 45)]
-    private ?string $img1 = null;
-
-    #[ORM\Column(length: 45, nullable: true)]
-    private ?string $img2 = null;
-
-    #[ORM\Column(length: 45, nullable: true)]
-    private ?string $img3 = null;
+    #[ORM\OneToMany(mappedBy: 'category', targetEntity: Product::class, orphanRemoval: true)]
+    private Collection $products;
 
     public function __construct()
     {
-        $this->orderDetails = new ArrayCollection();
+        $this->products = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -109,14 +65,14 @@ class Category
         return $this;
     }
 
-    public function getAbstract(): ?string
+    public function getImg(): ?string
     {
-        return $this->abstract;
+        return $this->img;
     }
 
-    public function setAbstract(string $abstract): self
+    public function setImg(?string $img): self
     {
-        $this->abstract = $abstract;
+        $this->img = $img;
 
         return $this;
     }
@@ -126,207 +82,39 @@ class Category
         return $this->description;
     }
 
-    public function setDescription(string $description): self
+    public function setDescription(?string $description): self
     {
         $this->description = $description;
 
         return $this;
     }
 
-    public function getQuantity(): ?int
-    {
-        return $this->quantity;
-    }
-
-    public function setQuantity(int $quantity): self
-    {
-        $this->quantity = $quantity;
-
-        return $this;
-    }
-
-    public function getPrice(): ?float
-    {
-        return $this->price;
-    }
-
-    public function setPrice(float $price): self
-    {
-        $this->price = $price;
-
-        return $this;
-    }
-
-    public function getMinPlayers(): ?int
-    {
-        return $this->min_players;
-    }
-
-    public function setMinPlayers(int $min_players): self
-    {
-        $this->min_players = $min_players;
-
-        return $this;
-    }
-
-    public function getMaxPlayers(): ?int
-    {
-        return $this->max_players;
-    }
-
-    public function setMaxPlayers(?int $max_players): self
-    {
-        $this->max_players = $max_players;
-
-        return $this;
-    }
-
-    public function getMinimumAge(): ?int
-    {
-        return $this->minimum_age;
-    }
-
-    public function setMinimumAge(?int $minimum_age): self
-    {
-        $this->minimum_age = $minimum_age;
-
-        return $this;
-    }
-
-    public function getDuration(): ?\DateTimeInterface
-    {
-        return $this->duration;
-    }
-
-    public function setDuration(?\DateTimeInterface $duration): self
-    {
-        $this->duration = $duration;
-
-        return $this;
-    }
-
-    public function getEditor(): ?string
-    {
-        return $this->editor;
-    }
-
-    public function setEditor(?string $editor): self
-    {
-        $this->editor = $editor;
-
-        return $this;
-    }
-
-    public function getTheme(): ?string
-    {
-        return $this->theme;
-    }
-
-    public function setTheme(?string $theme): self
-    {
-        $this->theme = $theme;
-
-        return $this;
-    }
-
-    public function getMecanism(): ?string
-    {
-        return $this->mecanism;
-    }
-
-    public function setMecanism(?string $mecanism): self
-    {
-        $this->mecanism = $mecanism;
-
-        return $this;
-    }
-
-    public function getCreatedAt(): ?\DateTimeImmutable
-    {
-        return $this->created_at;
-    }
-
-    public function setCreatedAt(\DateTimeImmutable $created_at): self
-    {
-        $this->created_at = $created_at;
-
-        return $this;
-    }
-
-    public function getCategory(): ?Category
-    {
-        return $this->category;
-    }
-
-    public function setCategory(?Category $category): self
-    {
-        $this->category = $category;
-
-        return $this;
-    }
-
     /**
-     * @return Collection<int, OrderDetail>
+     * @return Collection<int, Product>
      */
-    public function getOrderDetails(): Collection
+    public function getProducts(): Collection
     {
-        return $this->orderDetails;
+        return $this->products;
     }
 
-    public function addOrderDetail(OrderDetail $orderDetail): self
+    public function addProduct(Product $product): self
     {
-        if (!$this->orderDetails->contains($orderDetail)) {
-            $this->orderDetails->add($orderDetail);
-            $orderDetail->setCategoryId($this);
+        if (!$this->products->contains($product)) {
+            $this->products->add($product);
+            $product->setCategory($this);
         }
 
         return $this;
     }
 
-    public function removeOrderDetail(OrderDetail $orderDetail): self
+    public function removeProduct(Product $product): self
     {
-        if ($this->orderDetails->removeElement($orderDetail)) {
+        if ($this->products->removeElement($product)) {
             // set the owning side to null (unless already changed)
-            if ($orderDetail->getCategoryId() === $this) {
-                $orderDetail->setCategoryd(null);
+            if ($product->getCategory() === $this) {
+                $product->setCategory(null);
             }
         }
-
-        return $this;
-    }
-
-    public function getImg1(): ?string
-    {
-        return $this->img1;
-    }
-
-    public function setImg1(string $img1): self
-    {
-        $this->img1 = $img1;
-
-        return $this;
-    }
-
-    public function getImg2(): ?string
-    {
-        return $this->img2;
-    }
-
-    public function setImg2(?string $img2): self
-    {
-        $this->img2 = $img2;
-
-        return $this;
-    }
-
-    public function getImg3(): ?string
-    {
-        return $this->img3;
-    }
-
-    public function setImg3(?string $img3): self
-    {
-        $this->img3 = $img3;
 
         return $this;
     }
